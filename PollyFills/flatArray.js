a8e9dc914cd32f40aadc0643449c5a56e71aeb95
Imran <imali@deloitte.com>
2025-07-05 00:16:44 +0530

    flat Array

--- PollyFills/flatArray.js
+++ PollyFills/flatArray.js
@@ -1,16 +1,16 @@
 const arrone = [1, 2, [3, 4, [5, 6], 7, 8], 9, 10];
 
-Array.prototype.flatOne =  function (deep) {
+Array.prototype.flatOne = function (deep) {
   let arr = [];
   let current = deep;
   function deepFalt(num) {
-    if (typeof num == "object" && current>0) {
+    if (typeof num == "object" && current > 0) {
       current--;
       for (let i = 0; i < num.length; i++) {
-        deepFalt(num[i])
+        deepFalt(num[i]);
       }
     } else {
-      arr.push(num)
+      arr.push(num);
     }
   }
 
@@ -19,6 +19,27 @@ Array.prototype.flatOne =  function (deep) {
   }
 
   return arr;
-}
+};
 
-console.log(arrone.flatOne(3))
+console.log(arrone.flatOne(3));
+
+// another approach
+Array.prototype.flatTwo = function (
+  maxlevel = 1,
+  currentlevel = 1,
+  result = []
+) {
+  for (element of this) {
+    if (Array.isArray(element) && currentlevel <= maxlevel) {
+      element.flatTwo(maxlevel, currentlevel + 1, result);
+    } else {
+      result.push(element);
+    }
+  }
+  return result
+};
+
+console.log(arrone.flatTwo(3))
+console.log(arrone.flatTwo())
+console.log(arrone.flatTwo(0))
+
